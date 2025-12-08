24e8f608787655a1878bf695592d31dbd09c3732
fcorinaldesi <fcorinaldesi@unpaz.edu.ar>
2025-12-08 18:48:58 -0300

    feat: :sparkles: se agrego el punto de entrada

---
+++ predict-service/src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const predictRoutes = require('./routes/predictRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3002; // Usamos el puerto 3002
+
+app.use(express.json());
+app.use(cors());
+
+// Rutas
+app.use('/predict', predictRoutes);
+
+// Health check
+app.get('/health', (req, res) => res.send('Predict Service OK'));
+
+app.listen(PORT, () => {
+    console.log(`Predict Service (con Caché) escuchando en puerto ${PORT}`);
+});
